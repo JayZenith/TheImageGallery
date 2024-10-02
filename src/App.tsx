24250ad0b35e75e59bfadebed93184cba7cc8655
JayZenith <jayzenith248@gmail.com>
2024-10-02 14:47:48 -0700

    able to delete images by index

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Lightbox from 'yet-another-react-lightbox'
-import { slides } from './data'
+//import { slides } from './data'
 import 'yet-another-react-lightbox/styles.css'
 import { Captions, Download, Fullscreen } from 'yet-another-react-lightbox/plugins'
 import Counter from "yet-another-react-lightbox/plugins/counter";
@@ -13,18 +13,42 @@ function App() {
   const [open, setOpen] = useState<boolean>(false)
   const [index, setIndex] = useState<number>(-1)
   const [theUrl, setTheUrl] = useState<string>("")
+  const [render, setRender] = useState<boolean>(false)
+  const [slides, setSlides] = useState<any>([])
+  const [theUrlLength, setTheUrlLength] = useState<number>(0)
+  const [errMsg, setErrMsg] = useState<string>("")
+  const [showDelete, setShowDelete] = useState<boolean>(false)
 
+
+  
+  useEffect(()=>{
+    setRender(false);
+    var retrievedObject = localStorage.getItem('theKey');
+    //console.log(JSON.parse(retrievedObject || '{}'));
+    setSlides(JSON.parse(retrievedObject || '{}'))
+    console.log(slides)
+    setRender(true);
+  },[])
 
 
   const imageSubmit = (e: any) => {
     e.preventDefault();
+    if(theUrlLength === 0){
+      setErrMsg('Cant leave empty')
+      return;
+    }
     let theArray = slides;
     theArray.push({src:theUrl, title:"a title", description: "descr"})
+  
+    localStorage.setItem("theKey", JSON.stringify(theArray));
     setTheUrl('')
+    setRender(true);
   }
 
   const handleUrl = (e: any) => {
+    setErrMsg('')
     setTheUrl(e.target.value);
+    setTheUrlLength(e.target.value.length)
   }
 
   return (
@@ -38,34 +62,41 @@ function App() {
     </header>
     <div className='addUrl'>
       <form onSubmit={imageSubmit}>
+        <p>{errMsg}</p>
         <input type="text" placeholder="" value={theUrl} onChange={(e)=>handleUrl(e)}/>
+        <p>{theUrlLength}</p>
         <button type="submit" className='submitButton'>Submit</button>
-
       </form>
+      <button className='showDeleteButton' onClick={()=>setShowDelete(!showDelete)}>Show</button>
     </div>
-    {/*<button onClick={()=>setOpen(true)}>Open Box</button>*/}
-    <Images data={slides} onClick={(curIdx)=>setIndex(curIdx)} />
+    
+    {render ? (
+    <>
+      <Images passed={showDelete} data={slides} onClick={(curIdx)=>setIndex(curIdx)} />
 
-    <div style={{ width: "100%", maxWidth: "900px", aspectRatio: "3 / 2" }}>
-      <Lightbox
-        plugins={[Captions, Counter, Download, Fullscreen]}
+      <div style={{ width: "100%", maxWidth: "900px", aspectRatio: "3 / 2" }}>
+        <Lightbox
+          plugins={[Captions, Counter, Download, Fullscreen]}
+          
+          counter ={{ container: {style: { top: "unset", bottom: 0 } }}}
+          captions={{
+            showToggle: true,
+            descriptionTextAlign: 'center'
+          }}
+          carousel={{ padding: 64 }}
+          
+          slides={slides}
+          //open={open}
+          //close={()=>setOpen(false)}
+          index={index}
+          open={index >= 0}
+          close={()=>setIndex(-1)}
         
-        counter ={{ container: {style: { top: "unset", bottom: 0 } }}}
-        captions={{
-          showToggle: true,
-          descriptionTextAlign: 'center'
-        }}
-        carousel={{ padding: 64 }}
-        
-        slides={slides}
-        //open={open}
-        //close={()=>setOpen(false)}
-        index={index}
-        open={index >= 0}
-        close={()=>setIndex(-1)}
-      
-      />
-    </div>
+        />
+      </div>
+    </>) : <></>}
+
+  
     </>
   )
 }
--- src/Images.tsx
+++ src/Images.tsx
@@ -7,7 +7,9 @@ interface ImagesProps{
         description: string;
     }[];
     onClick: (idx: number) => void;
+    passed: boolean
 }
+
 
 const Images: FC<ImagesProps> = (props) => {
   const { data, onClick } = props;
@@ -16,11 +18,25 @@ const Images: FC<ImagesProps> = (props) => {
     onClick(idx)
   }
 
+  const deletePic = (idx : number) => {
+    console.log(idx)
+    var retrievedObject = localStorage.getItem('theKey');
+    let arr = JSON.parse(retrievedObject || '{}')
+    console.log(arr)
+    
+    arr.splice(idx, 1); 
+    console.log(arr)
+    localStorage.setItem("theKey", JSON.stringify(arr));
+  }
+
   return (
     <div className="images-container">
         {data.map((slide, index ) => (
+            <div>
+            {props.passed ? (<span className="abs" onClick={()=>deletePic(index)}>X</span> ) : <></>}
             <div onClick={()=>handleClickImage(index)} key={index} className="image">
                 <img src={slide.src} alt={slide.description} />
+            </div>
             </div>
 
         ))}
